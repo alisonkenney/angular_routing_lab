bdd79be6d8f1fd1572c569cb39c57d6ecbfa02b6
Alison Kenney <alisonkenney1@gmail.com>
2016-08-17 11:38:58 -0600

    all challenges completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('wineApp', []);
+var app = angular.module('wineApp', ['ngRoute']);
 
 console.log('Angular is working.');
 
@@ -6,50 +6,88 @@ console.log('Angular is working.');
 // ROUTES //
 ////////////
 
+ app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/', {
+      // template: 'Home!',
+      templateUrl: '/templates/wines-index.html',
+      controller: 'WinesIndexCtrl'
+    })
+    .when('/wines/:id', { // the "id" parameter 
+    templateUrl: 'templates/wines-show.html',
+    controller: 'WinesShowCtrl'
+    });
+    $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+});
+});
+
+
 
 /////////////////
 // CONTROLLERS //
 /////////////////
 
-app.controller('WinesIndexCtrl',function($scope){
-  console.log("Wine Index")
-})
+WinesIndexCtrl.$inject = ['$scope', '$http'];
+app.controller('WinesIndexCtrl', WinesIndexCtrl);
+function WinesIndexCtrl($scope, $http){
+    this.$inject = ['http'];
+     $http.get('http://daretoexplore.herokuapp.com/wines/')
+     .then(function(response) {
+          $scope.wines = response.data;         
+     });  
+}
 
-app.controller('WinesShowCtrl',function($scope){
-  console.log("Wine Show")
-})
+WinesShowCtrl.$inject = ['$scope', '$http', '$routeParams'];
+app.controller('WinesShowCtrl', WinesShowCtrl);
+function WinesShowCtrl($scope, $http, $routeParams){
+     $http.get('http://daretoexplore.herokuapp.com/wines/' + $routeParams.id)
+     .then(function(response) {
+          $scope.wine = response.data;         
+     });  
+}      
+
+
+// WinesIndexCtrl.$inject = ['$scope', 'WineService'];
+// app.controller('WinesIndexCtrl', WinesIndexCtrl);
+
+// function WinesIndexCtrl($scope, WineService){
+//   console.log("Wine Index");
+//   $scope.hello = "wine index controller is working!";
+//   // $scope.wines = ALL_WINES; using array at the bottom
+//   $scope.wines = WineService.query();
+// }
+
+// WinesShowCtrl.$inject = ['$scope', 'WineService', '$routeParams'];
+// app.controller('WinesShowCtrl', WinesShowCtrl);
+
+// function WinesShowCtrl($scope, WineService, $routeParams){
+//    $scope.wine = WineService.get($routeParams.id);
+// }
 
 ////////////
 // MODELS //
 ////////////
 
-app.factory('WineService', function(){
+// app.factory('WineService', function(){
 
-  var WineService = {};
+//   var WineService = {};
 
-  WineService.query = function(){
-    return ALL_WINES;
-  }
+//   WineService.query = function(){
+//     return ALL_WINES;
+//   };
 
-  WineService.get = function(id){
-    var id = parseInt(id);
-    return ALL_WINES.find(function(wine){
-      return wine.id == id;
-    });
-  }
+//   WineService.get = function(id){
+//     var id = parseInt(id);
+//     return ALL_WINES.find(function(wine){
+//       return wine.id == id;
+//     });
+//   };
 
-  return WineService;
+//   return WineService;
 
-})
-
-
-
-
-
-
-
-
-
+// });
 
 
 /*
